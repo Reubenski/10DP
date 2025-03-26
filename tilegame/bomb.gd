8c1444b679d4216962e1e8264e435b9cb6097292
Reubenski <38727408+Reubenski@users.noreply.github.com>
2025-03-26 17:58:42 +0000

    added bomb/tileset interaction
    import the tileset node, converts bombs global coords to map coords

--- tilegame/bomb.gd
+++ tilegame/bomb.gd
@@ -1,20 +1,31 @@
 extends Area2D
 
+
+
 var speed = 400
 var screen_size
 
+
+
 func _ready():
 	screen_size = get_viewport_rect().size
+
+
+func gettingmap(pos):
+
+	var tiles = get_node("/root/Main/map").get_child(1) 
+	
+	tiles.erase_cell(tiles.local_to_map(tiles.to_local(pos)))
+
+
 
 func _process(delta):
 	position.y += speed * delta
 	if position.y > screen_size.y:
 		queue_free()
+	gettingmap(global_position)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
-		area.queue_free()
-		queue_free()
-	elif area.is_in_group("building"):
 		area.queue_free()
 		queue_free()
