eefe6fe4c719e1eab939dfa3efd6c71a8b0e1973
Ashley Ray <ashley_ray@myyahoo.com>
2025-03-28 14:30:03 +0000

    Map mouseclick handling

--- tilegame/map.gd
+++ tilegame/map.gd
@@ -2,4 +2,12 @@ extends Node2D
 
 
 func _ready():
-    pass
+	pass
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		var pos = event.position
+		var position = $CityMap.local_to_map($CityMap.to_local(pos));
+		if event.pressed:
+			print(position)
+			$CityMap.set_cell(position,0,Vector2i(0,0))
