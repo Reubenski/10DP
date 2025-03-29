37308247743e613561b5ad9f85f995caf48a1c6a
Reubenski <38727408+Reubenski@users.noreply.github.com>
2025-03-29 14:01:28 +0000

    expanded viewport and adjusted x origin spawns

--- tilegame/saucer.gd
+++ tilegame/saucer.gd
@@ -6,12 +6,17 @@ var bomb_counter = 0
 @export var speed = 200
 @export var drop_distance = 128
 @export var margins = 100
+@export var uioffset=200
+@export var square_size = 800
 var target_y = drop_distance
 var screen_size
+var screen_offset
 var rng = RandomNumberGenerator.new()
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	#screen_size = get_viewport_rect().size
+	screen_size = Vector2(square_size +uioffset, square_size)
+	screen_offset=Vector2(uioffset,0)
 	bomb_counter = bomb_time + rng.randf_range(0, bomb_time)
 	$AnimatedSprite2D.play()
 	
@@ -19,11 +24,11 @@ func _process(delta):
 	if position.y < target_y:
 		position.y += abs(speed) * delta
 		return
-	if (speed > 0 and position.x > screen_size.x - margins) || (speed < 0 and position.x < margins):
+	if (speed > 0 and position.x > (screen_size.x)- margins ) || (speed < 0 and position.x < (margins + uioffset)):
 		speed = -speed
 		target_y = min(target_y+drop_distance,screen_size.y/2)
 	position.x += speed * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	position = position.clamp(screen_offset, screen_size)
 	if bomb_counter <= 0:
 		drop_bomb()
 	bomb_counter -= delta
--- tilegame/tank.gd
+++ tilegame/tank.gd
@@ -4,14 +4,19 @@ extends Area2D
 @export var acceleration = 300
 @export var reload_time = 1
 @export var projectile : PackedScene
+@export var uioffset=200
+@export var square_size = 800
 var velocity = Vector2.ZERO
 var screen_size
+var screen_offset
 var reload_counter = 0;
 
+
 func _ready():
-	screen_size = get_viewport_rect().size
+	screen_size = Vector2(square_size  +uioffset, square_size)
+	screen_offset=Vector2(uioffset,0)
 	var spawn = Vector2.ZERO
-	spawn.x = screen_size.x/2
+	spawn.x = uioffset + screen_size.x/2
 	spawn.y = screen_size.y-32
 	position = spawn
 	$AnimatedSprite2D.play()
@@ -33,4 +38,4 @@ func _process(delta):
 	elif velocity.x < 0:
 		velocity.x += acceleration * delta
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	position = position.clamp(screen_offset, screen_size)
