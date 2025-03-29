8a5507c6161e7f1fb4ab4b67847f12b73aaf6c27
Ashley Ray <ashley_ray@myyahoo.com>
2025-03-29 13:24:22 +0000

    Cool Explosion

--- tilegame/bomb.gd
+++ tilegame/bomb.gd
@@ -1,21 +1,24 @@
 extends Area2D
 
-
-
+@export var explosion : PackedScene
 var speed = 400
 var screen_size
-
-
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	$AnimatedSprite2D.play()
+
+func explode():
+	var p = explosion.instantiate()
+	get_parent().add_child(p)
+	p.transform = $ExplosionPoint.global_transform
 
 func gettingmap(pos):
 	var tiles = get_node("/root/Main/map").get_child(1) 
 	var position = tiles.local_to_map(tiles.to_local(pos));
 	var tiledata = tiles.get_cell_tile_data(position)
 	if tiledata != null:
+		explode()
 		tiles.erase_cell(position)
 		queue_free()
 
@@ -27,5 +30,6 @@ func _process(delta):
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
+		explode()
 		area.queue_free()
 		queue_free()
--- tilegame/projectile.gd
+++ tilegame/projectile.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
 var speed = 400
+@export var explosion : PackedScene
 
+func explode():
+	var p = explosion.instantiate()
+	get_parent().add_child(p)
+	p.transform = $ExplosionPoint.global_transform
+	
 func _process(delta):
 	position.y -= speed * delta
-	if position.y < 0:
+	if position.y < -500:
 		queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("aliens"):
+		explode()
 		area.queue_free()
 		queue_free()
