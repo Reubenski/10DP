42e36de4b1efdb0460734843101126abbce95bac
Reubenski <38727408+Reubenski@users.noreply.github.com>
2025-04-01 16:48:51 +0100

    Update map.gd

--- tilegame/map.gd
+++ tilegame/map.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var tile = Vector2i(0,0)
+
 var bricks =  10
 var people =10
 var metal =10
@@ -14,6 +16,9 @@ func _input(event):
 	if event is InputEventMouseButton:
 		var pos = event.position
 		var position = $CityMap.local_to_map($CityMap.to_local(pos));
+		
 		if event.pressed:
-			print(position)
-			$CityMap.set_cell(position,0,Vector2i(0,0))
+			var itemlist = get_node("/root/Main/SidebarUi").get_child(0)
+			
+			$CityMap.set_cell(position,0,Vector2i(itemlist.get_selected_items().get(0)*4,0))
+			
