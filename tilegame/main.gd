75c858fa9966db75dd3bba3a79a27ffed1b1d8b7
Ashley Ray <ashley_ray@myyahoo.com>
2025-04-12 16:00:17 +0100

    Reset score on restart

--- tilegame/main.gd
+++ tilegame/main.gd
@@ -5,6 +5,10 @@ extends Node
 var waveNo = 0
 var wave = [];
 var waveTimer = 0
+
+func _ready():
+	Global.score = 0
+
 func _process(delta):
 	if get_tree().get_nodes_in_group("aliens").size() == 0 and wave.size() == 0:
 		#modulate resourcelistui to green
