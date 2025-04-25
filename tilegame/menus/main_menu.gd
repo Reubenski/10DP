ac6a4fcbc66b017bbf3b637d0fa9f4b5df4c4354
Reubenski <38727408+Reubenski@users.noreply.github.com>
2025-04-25 18:39:33 +0100

    intro added

--- tilegame/menus/main_menu.gd
+++ tilegame/menus/main_menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
-
+func _ready():
+	var tween = create_tween()
+	tween.tween_property($ColorRect/introtext, "self_modulate", Color.TRANSPARENT, 3)
+	tween.tween_property($ColorRect, "self_modulate", Color.TRANSPARENT, 1)
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
